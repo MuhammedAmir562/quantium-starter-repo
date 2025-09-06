529b07daebddc7afb2a9197064fffa312e0a0bb2
Muhammed <fkddku@gmail.com>
2025-09-06 15:17:02 +0100

    task 2- filter db to only show sales, date and region of pink morsels

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
 #read all csv files
-df1 = pd.read_csv('daily_sales_data_0.csv')
-df2 = pd.read_csv('daily_sales_data_1.csv')
-df3 = pd.read_csv('daily_sales_data_2.csv')
+df1 = pd.read_csv('data/daily_sales_data_0.csv')
+df2 = pd.read_csv('data/daily_sales_data_1.csv')
+df3 = pd.read_csv('data/daily_sales_data_2.csv')
 
 #combine all csv files into one file
 sales_df = pd.concat([df1, df2, df3])
